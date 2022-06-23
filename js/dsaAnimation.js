42475bc2cfd7fffae23ab255c4305eedff81ce48
Aashray <aashray446@gmail.com>
2022-06-23 23:18:33 +0530

    <Feature>: [Completed Stack Features]

--- js/dsaAnimation.js
+++ js/dsaAnimation.js
@@ -11,57 +11,56 @@ const createElement = function(value) {
     else {
         element.innerHTML = value
     }
-    container.appendChild(element)        
+    container.prepend(element)        
 }
 
-const initDisplay = function() {
-
-    //Resetting if already initialized
-    if( container.childElementCount > 10) {
-        container.innerHTML = ""
-    }
-
-    createElement()
+const getHead = function() {
     
-  
-    //Related Animations 
-    anime({
-        targets: '.' + structure,
-        scale: [
-          {value: .1, easing: 'easeOutSine', duration: 500},
-          {value: 1, easing: 'easeInOutQuad', duration: 1200}
-        ],
-        delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
-      });
-}
-
-const pushStack = function(value){
-    
-    createElement();
+    const list = document.getElementsByClassName(structure)
+    return list[0]
 
 }
 
 
+
+
+// ------------------ Implementation of stack with animation  -------------------------
 // Stack Implementation with array
 class Stack {
    
     dataArray = new Array();
 
-    constructor(value) {
-        this.dataArray.push(value)
-    }
-
     push(value) {
+
+        if(this.dataArray.length > 12) {
+            window.alert("Stack out of range")
+        }
+        else {
+
+        document.getElementById('pushingData').innerHTML = value
+        createElement(value)
         this.dataArray.push(value)
         return
+        }
     }
 
     pop() {
-        const list = document.getElementsByClassName('box')
-        list[list.length-1].remove()
-        return this.dataArray.pop()
+        const list = document.getElementsByClassName(structure)
+        list[0].remove()
+        document.getElementById('popingData').innerHTML = this.dataArray.pop()
     }
 
+    reset = function() {
+        container.innerHTML = ''    
+    }
 
     
 }
+
+// Function that will be used by the browser
+let stack = new Stack()
+
+
+
+
+
