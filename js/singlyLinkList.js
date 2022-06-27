878971bc185983ea6eb5d745cfa27666108e8bab
ronakdudhani <ronakdudhani@gmail.com>
2022-06-27 10:12:03 +0530

    <Singly-Linked-List>:[Implemented basic function of Singly linked list]

--- js/singlyLinkList.js
+++ js/singlyLinkList.js
@@ -1,6 +1,15 @@
 // Please Make LinkList Implementation 
 // All possible Functions 
+class node {
+    // constructor
+    constructor(value) {
+        this.value = value;
+        this.next = null
+    }
+}
+
 class SinglyLinkList {
+ 
     constructor() {
         this.head = null;
         this.size = 0;
@@ -26,7 +35,10 @@ class SinglyLinkList {
         let node = new node(value);
         let current, previous;
         current = this.head;
-        if(index == 0) {
+        if(index == null){
+            insert(value);
+        }
+        if(index === 0) {
             node.next = this.head;
             this.head = node;
         }
@@ -42,6 +54,63 @@ class SinglyLinkList {
             previous.next = node;
         }
         this.size++;
+    }
+
+    deleteValue = function(value) {
+        let current = this.head;
+        let prev = null;
+        while(current != null){
+            if(current.value == value){
+                if(prev == null){
+                    this.head = current.next;
+                }
+                else{
+                    prev.next = current.next;
+                }
+                this.size--;
+                return;
+            }
+            prev = current;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    deleteIndex = function(index) {
+
+        if(index<0 || index>this.size){return alert("Insert a Valid Index")};
+        let current = this.head;
+        let prev = null;
+        let position = 0;
+        if(index === 0){
+            this.head = current.next;
+        }
+        else{
+            while(position < index){
+                position++;
+                prev = current;
+                current = current.next;
+            }
+            prev.next = current.next;
+        }
+        this.size--;
+        return current.value;
+    }
+
+    search = function(value){
+
+        let index = 0;
+        let current = this.head;
+
+        while(current != null){
+            if(current.value===value){
+                return count;
+            }
+            count++;
+            current = current.next;
+        }
+        return -1;
+
     }
 
 }
