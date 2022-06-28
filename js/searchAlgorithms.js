13cb513c5697e34591375850fdae663f650836c7
Akshaj Sr <83636572+Akshaj000@users.noreply.github.com>
2022-06-29 00:26:41 +0530

    Search (#21)
    * <algorithms>:[implement basic search js]
    
    * <algorithms>:[implement basic search features]

--- js/searchAlgorithms.js
+++ js/searchAlgorithms.js
@@ -1,45 +1,43 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function bblSort(arr){
+    for(var i = 0; i < arr.length; i++){
+        for(var j = 0; j < ( arr.length - i -1 ); j++){
+            if(arr[j] > arr[j+1]){
+                var temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j+1] = temp
+            }
+        }
+    }
+    return arr;
+}
+
 class SearchAlgorithms {
     container = document.getElementById('animation-container')
+    display = document.getElementById('search-box')
+    queinf = document.getElementById('queueinfo')
+
     structure = 'queue'
 
-    createElement = (data) => {
+    createElement = (value) => {
         const element = document.createElement('div');
         element.classList.add(this.structure)
-        element.style.backgroundColor = data?.color
-        element.innerHTML = `<p style="color: white; font-size: xx-large">${data?.value}</p>`
-        element.classList.add('circlerow')
+        element.innerHTML = `<p style="color: black; font-size: xx-large">${value}</p>`
         return element
-    }
-
-    getRandomColor = () => {
-        var trans = '0.3';
-        var color = 'rgba(';
-        for (var i = 0; i < 3; i++) {
-            color += Math.floor(Math.random() * 255) + ',';
-        }
-        color += trans + ')';
-        return color;
-    }
-
-    wrapFR = (value, isfront) => {
-        if(isfront) {
-            return `<p class="text-center">Front is ${value}</p>`
-        }
-        return `<p class="text-center">Rear is ${value}</p>`
     }
 
     array = new Array();
     length;
-    searchtype = 'binary';
-
-    init(length) {
-        this.length = length;
-    }
+    searchtype = 'linear';
 
     render(){
+        this.display.style.visibility = 'hidden';
         this.container.innerHTML = '';
         for (let i = 0; i < this.array.length; i++) {
-            this.container.append(this.createElement({'value': this.array[i], 'color': this.getRandomColor()}))
+            this.container.append(this.createElement(this.array[i]));
         }
     }
 
@@ -48,16 +46,19 @@ class SearchAlgorithms {
             value = Math.floor((Math.random() * 99) + 1);
         }
         this.array.push(value);
-        if(this.searchtype === 'binary') {
-            this.array.sort();
+        this.render();
+    }
+
+    generateRandomArray(){
+        length=20;
+        this.array = new Array();
+        for(let i=0;i<length;i++){
+            this.add(Math.floor((Math.random() * 99) + 1));
         }
         this.render();
     }
 
     remove(index) {
-        if(this.searchtype === 'binary') {
-            this.array.sort();
-        }
         if (index !== -1) {
             this.array.pop(index);
             this.container.lastChild.remove();
@@ -66,43 +67,80 @@ class SearchAlgorithms {
 
     }
 
-    search(target) {
+    async search(target) {
+        if(this.array.length==0) {
+            this.queinf.getElementsByTagName('p')[0].innerHTML = "Please add some elements to the Array !";
+            this.queinf.style.display = 'block';
+            return;
+        }
+        if(target=="") {
+            this.queinf.getElementsByTagName('p')[0].innerHTML = "Please enter a value to search !";
+            this.queinf.style.display = 'block';
+            return;
+        }
+        this.queinf.style.display = 'none';
         let index = -1;
         if (this.searchtype === 'linear') {
-            index = this.linearSearch(this.array, target);
+            index = await this.linearSearch(this.array, target);
         } else {
-            index = this.binarySearch(this.array, target);
+            index = await this.binarySearch(this.array, target);
+        }
+        if (index !== -1) {
+            this.display.style.visibility = 'visible';
+            this.display.innerHTML = `<p class="text-center"> index is ${index}</p>`
+            this.container.children[index].style.backgroundColor = 'green';
+        } else {
+            this.display.style.visibility = 'visible';
+            this.display.innerHTML = `<p class="text-center"> Element not found</p>`
         }
         return index;
     }
 
-    linearSearch(array, target) {
+    async linearSearch(array, target) {
+        this.render()
         for (let i = 0; i < array.length; i++) {
-            if (array[i] === target) {
+            this.display.style.visibility = 'visible';
+            this.display.innerHTML = `<p class="text-center">Checking element at index ${i}.</p>`
+            this.container.children[i].style.backgroundColor = 'orange';
+            if (array[i] == target) {
                 return i;
             }
+            await sleep(300);
+            this.container.children[i].style.backgroundColor = '#24b4a6df';
         }
+
         return -1;
     }
 
-    binarySearch(array, target) {
+    async binarySearch(array,target) {
+        array = bblSort(this.array);
+        this.array=array;
+        this.render()
         let left = 0;
         let right = array.length - 1;
+        let mid = Math.floor((left + right) / 2);
         while (left <= right) {
-            let mid = Math.floor((left + right) / 2);
-            if (array[mid] === target) {
+            this.display.style.visibility = 'visible';
+            this.display.innerHTML = `<p class="text-center">Mid is ${mid}, start is ${left}, end is ${right}</p>`
+            this.container.children[mid].style.backgroundColor = 'orange';
+            console.log(target, array[mid])
+            if (array[mid] == target) {
                 return mid;
-            } else if (array[mid] < target) {
-                left = mid + 1;
-            } else {
+            } else if (array[mid] > target) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
+            await sleep(500);
+            this.container.children[mid].style.backgroundColor = '#24b4a6df';
+            mid = Math.floor((left + right) / 2);
         }
         return -1;
     }
 
     reset() {
         this.array = new Array();
+        this.container.innerHTML = '';
     }
 }
 
