310b8107281327a9c0bf078ff6ea1fd2a380fab7
Sumitha <saisumitha0610@gmail.com>
2022-07-14 01:38:28 +0530

    <Feature> : [Completed Doubly Linked List without animation]

--- js/doubleLinkedList.js
+++ js/doubleLinkedList.js
@@ -23,15 +23,15 @@ const createNode = function(prevData, currentData, nextData) {
     data.innerHTML = currentData;
 
     //Next Address
-    var next  = document.createElement('div');
+    var next = document.createElement('div');
     next.classList.add('data');
-   
-    if( nextData == null) {
+    if( nextData === null) {
         nextData.innerHTML = ' - '
     }
     else  {
-        nextData.innerHTML =  nextData;
+        next.innerHTML =  nextData;
     }
+    
 
     element.appendChild(prev)
     element.appendChild(data)
@@ -41,20 +41,77 @@ const createNode = function(prevData, currentData, nextData) {
 }
 
 const addArrow = function() {
-    const element = document.createElement('div').classList.add('arrow-6')
+    const element = document.createElement('div');
+    element.classList.add('arrow-6');
     container.append(element)
 }
 
+const Pointer = function() {
+    const element = document.createElement('div');
+    element.classList.add('pointer');
+    return element;
+}
 
 
-// All possible Functions 
+   // Pointer animation related information
+    // move pointer to next node
+    movePointerNext = function(){
+        for (let i = 0; i < container.childElementCount; i++) {
+            if(container.children[i].classList.contains('arrow-6')){
+                continue;
+            }
+            if(container.children[i].lastChild.classList.contains('pointer')){
+                container.children[i].removeChild(container.children[i].lastChild)
+                container.children[i+2].appendChild(Pointer())
+                return;
+            }
+        }
+    }
+
+   movePointerPrev = function(){
+        for (let i = 0; i < container.childElementCount; i++) {
+            if(container.children[i].classList.contains('arrow-6')){
+                continue;
+            }
+            if(container.children[i].lastChild.classList.contains('pointer')){
+                container.children[i].removeChild(container.children[i].lastChild)
+                container.children[i-2].appendChild(Pointer())
+                return;
+            }
+        }
+    }
+
+    getPresentPointerPos = function(){
+        for (let i = 0; i < container.childElementCount; i++) {
+            if(container.children[i].classList.contains('arrow-6')){
+                continue;
+            }
+            if(container.children[i].lastChild.classList.contains('pointer')){
+                return i;
+            }
+        }
+    }
+
+    //return a random number between 0-90
+    const randomNumber = function() {
+        return Math.floor(Math.random() * (90 - 0 + 1)) + 0;
+    }
+
+
+
+
+//  ------------------------------------ DOUBLY LINKED LIST IMPLEMENTATION ------------------------------------
 class node{
     constructor(value){
         this.value = value;
         this.next = null;
         this.prev = null;
+        this.prevMemory = null;
+        this.nextMemory= null;
+        this.myMemory = null;
     }
 }
+
 
 class DoublyLinkedList {
 
@@ -64,31 +121,77 @@ class DoublyLinkedList {
         this.size = 0;
     }
 
-   insert = function(value){
 
-   }
+  //insert in doubly linked List
+    insert(value) {
+        const newNode = new node(value);
+        newNode.myMemory = randomNumber();
 
-    insertAtIndex = function(value, index){
-        if(index < 0 || index > this.size){
-            return console.log("Invalid Index");
+        // if list is empty
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+            this.size++;
+
+            //Psuedo Memory Address
+            newNode.nextMemory = " - "
+            newNode.prevMemory = " - "
+        
+            // Drawing the node
+            createNode(newNode.prevMemory, value, newNode.nextMemory)
+            //add pointer to the node
+            container.lastChild.appendChild(Pointer())
+            return;
         }
-        let node = new node(value);
-        let current, previous;
-        current = this.head;
-        let position = 0;
-        while(position<index){
-            position++;
-            current = current.next;
-        }
-        previous = current.prev;
-        previous.next = node;
-        node.prev = previous;
-        node.next = current;
-        current.prev = node;
+
+
+        // if  list is not empty
+        this.tail.nextMemory = newNode.myMemory;
+        this.changePrevMemory(newNode.myMemory);
+        newNode.prevMemory = this.tail.myMemory
+        this.tail.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+        newNode.nextMemory = " - "
         this.size++;
+        addArrow();
+        createNode(newNode.prevMemory, value, newNode.nextMemory)
+        movePointerNext();
+        return;
     }
-   
+
+    //insert at index
+    insertAt(value, index) {
+        if (index > this.size) {
+            return;
+        }
+        if (index === 0) {
+            this.insert(value);
+            return;
+        }
+        const newNode = new node(value);
+        let current = this.head;
+        let count = 0;
+        while (count < index - 1) {
+            current = current.next;
+            count++;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        newNode.prev = current;
+        newNode.next.prev = newNode;
+        createNode(5, value, 5);
+        this.size++;
+        return;
+    }
+
+
+    changePrevMemory(memory){
+        container.lastChild.children[2].innerHTML = memory
+    }
 
 }
+
+
 
 const dll = new DoublyLinkedList()
