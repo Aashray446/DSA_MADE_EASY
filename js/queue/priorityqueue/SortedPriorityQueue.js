4fb51aa28f559c24f87b5af625391cf90dc4b9aa
Akshaj <akshajsr000@gmail.com>
2022-07-14 15:53:17 +0530

    <deque>[style]

--- js/queue/priorityqueue/SortedPriorityQueue.js
+++ js/queue/priorityqueue/SortedPriorityQueue.js
@@ -1,26 +1,57 @@
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+}
+
 class SortedPriorityQueue {
-  constructor() {
-    this.queue = [];
-    this.length = 0;
-  }
+    constructor() {
+        this.dataArray = new Array();
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
 
-  enqueue(item, priority) {
-  if(this.isFull()){
-        return false;
-  }
-    this.queue.push({ item, priority });
-    this.queue.sort((a, b) => a.priority - b.priority);
-  }
+    isEmpty = () => {
+        return this.size === 0;
+    }
 
-  dequeue() {
-    return this.queue.shift().item;
-  }
+    insert = (key, value) => {
+        let node = new Node(key, value);
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            let current = this.head;
+            while (current.next !== null && current.next.key < key) {
+                current = current.next;
+            }
+            if (current.next === null) {
+                current.next = node;
+                node.previous = current;
+                this.tail = node;
+            } else {
+                node.next = current.next;
+                current.next.previous = node;
+                current.next = node;
+                node.previous = current;
+            }
+        }
+        this.size++;
+    }
 
-  isEmpty() {
-    return this.queue.length === 0;
-  }
+    delete = () => {
+        if(this.isEmpty){
+            return console.log("Queue is Empty");
+        }
+        let node = this.head;
+        if(this.head.next != null){
+            (this.head) = (this.head).next;
+        }
+        return
+    }
 
-  isFull() {
-    return this.queue.length === this.length;
-  }
 }
