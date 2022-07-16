3c56a90177d0da4c3732f9de0f7723d886d610ae
Aashray Katiyar <aashray446@gmail.com>
2022-07-17 00:04:29 +0530

    <Enhancement> : [Binary Tree UI completed]

--- js/binaryTree.js
+++ js/binaryTree.js
@@ -1,21 +1,85 @@
-//binary tree
-class binaryTree {
-
-
+class binarySearchTree {
     constructor(value) {
         this.value = value;
         this.left = null;
         this.right = null;
     }
-    insertLeft(value) {
-        this.left = new binaryTree(value);
-        return this.left;
-    }
-    insertRight(value) {
-        this.right = new binaryTree(value);
-        return this.right;
-    }
-
     
+    insert(value) {
+        if (value < this.value) {
+        if (this.left === null) {
+            this.left = new binarySearchTree(value);
+        } else {
+            this.left.insert(value);
+        }
+        } else {
+        if (this.right === null) {
+            this.right = new binarySearchTree(value);
+        } else {
+            this.right.insert(value);
+        }
+        }
+    }
+    
+    contains(value) {
+        if (value === this.value) {
+        return true;
+        } else if (value < this.value) {
+        if (this.left === null) {
+            return false;
+        } else {
+            return this.left.contains(value);
+        }
+        } else {
+        if (this.right === null) {
+            return false;
+        } else {
+            return this.right.contains(value);
+        }
+        }
+    }
+    
+    depthFirstTraversal(iteratorFunc, order) {
+        if (order === 'pre-order') {
+        iteratorFunc(this.value);
+        }
+    
+        if (this.left) {
+        this.left.depthFirstTraversal(iteratorFunc, order);
+        }
+    
+        if (order === 'in-order') {
+        iteratorFunc(this.value);
+        }
+    
+        if (this.right) {
+        this.right.depthFirstTraversal(iteratorFunc, order);
+        }
+    
+        if (order === 'post-order') {
+        iteratorFunc(this.value);
+        }
+    }
+    
+    breadthFirstTraversal(iteratorFunc) {
+        const queue = [this];
+        while (queue.length) {
+        const treeNode = queue.shift();
+        iteratorFunc(treeNode.value);
+        if (treeNode.left) {
+            queue.push(treeNode.left);
+        }
+        if (treeNode.right) {
+            queue.push(treeNode.right);
+        }
+
+        }
+    }
 }
+
+const treeContainer = document.getElementById('tree');
+const parentNode = document.createElement('ul');
+const dataNode = document.createElement('li');
+const data = document.createElement('a');
+
 
