8089000a2cfd1fcb7f1610d9b2517d74ef718c63
Ankit Sharma <sharmaa7861@gmail.com>
2022-07-17 14:31:24 +0530

    <debug>:[fixed error of peak and pop button]

--- js/stack.js
+++ js/stack.js
@@ -1,138 +1,127 @@
-//Container Variable Definition 
-const container = document.getElementById('animation-container')
-const structure = 'stack'
+//Container Variable Definition
+const container = document.getElementById("animation-container");
+const structure = "stack";
+const errorBox = document.getElementById("error");
 
-const createElement = function(value) {
-    const element = document.createElement('div');
-    element.classList.add('hidden')
-    element.classList.add(structure)
-    if(value==null) {
-        element.innerHTML = '-'
-    }   
-    else {
-        element.innerHTML = value
-    }
-    container.prepend(element)        
-}
-
+const createElement = function (value) {
+  const element = document.createElement("div");
+  element.classList.add("hidden");
+  element.classList.add(structure);
+  if (value == null) {
+    element.innerHTML = "-";
+  } else {
+    element.innerHTML = value;
+  }
+  container.prepend(element);
+};
 
 //  ------------------------------ ANIMATIONS FUNCTIONS -------------------------------------------------
 
-const movePushingElement = function() {
-    let target = document.getElementById('pushingData')
-    let arrayElements = document.getElementsByClassName('stack')
+const movePushingElement = function () {
+  let target = document.getElementById("pushingData");
+  let arrayElements = document.getElementsByClassName("stack");
 
-    //  Some Unkown Error BruteForcely Solved
-    let destination = arrayElements[1]
-    if(arrayElements.length == 1) {
-        destination = arrayElements[0]
-    }
+  //  Some Unkown Error BruteForcely Solved
+  let destination = arrayElements[1];
+  if (arrayElements.length == 1) {
+    destination = arrayElements[0];
+  }
 
-    let x = destination.offsetLeft - target.offsetLeft
-    let y = destination.offsetTop -  target.offsetTop
+  let x = destination.offsetLeft - target.offsetLeft;
+  let y = destination.offsetTop - target.offsetTop;
 
-    anime({
-        targets: target,
-        translateY : [
-            {value : y, easing : 'linear'}
-        ],
-        translateX : {
-            value : x,
-        },
-        complete: function() {
-            document.getElementsByClassName('stack')[0].classList.remove('hidden');
-            anime.set(target, {
-                translateX : 0,
-                translateY : 0
-            })
-        }
-      },
-      );
-}
+  anime({
+    targets: target,
+    translateY: [{ value: y, easing: "linear" }],
+    translateX: {
+      value: x,
+    },
+    complete: function () {
+      document.getElementsByClassName("stack")[0].classList.remove("hidden");
+      anime.set(target, {
+        translateX: 0,
+        translateY: 0,
+      });
+    },
+  });
+};
 
+const movePopingData = function () {
+  let destination = document.getElementById("popingData");
+  target = document.getElementsByClassName("stack")[0];
 
-const movePopingData = function() {
-    let destination = document.getElementById('popingData')
-    target = document.getElementsByClassName('stack')[0]
+  let x = destination.offsetLeft - target.offsetLeft;
+  let y = destination.offsetTop - target.offsetTop;
 
-    let x = destination.offsetLeft - target.offsetLeft
-    let y = destination.offsetTop -  target.offsetTop
-
-    anime({
-        targets: 'stack',
-        translateY : [
-            {value : y, easing : 'linear'}
-        ],
-        translateX : {
-            value : x,
-        },
-        complete: function() {
-            document.getElementsByClassName('stack')[0].classList.remove('hidden');
-            anime.set(target, {
-                translateX : 0,
-                translateY : 0
-            })
-        }
-      },
-      );
-}
-
+  anime({
+    targets: "stack",
+    translateY: [{ value: y, easing: "linear" }],
+    translateX: {
+      value: x,
+    },
+    complete: function () {
+      document.getElementsByClassName("stack")[0].classList.remove("hidden");
+      anime.set(target, {
+        translateX: 0,
+        translateY: 0,
+      });
+    },
+  });
+};
 
 // ------------------ Implementation of stack with animation  -------------------------
 // Stack Implementation with array
 class Stack {
-  
-    dataArray = new Array();
-    
-    push(value) {
+  dataArray = new Array();
 
-        if(value == 0){
-    document.getElementById('error').style.display = 'block';
-    return
-} else{
-    document.getElementById('error').style.display = 'none';
-} 
-
-        if(this.dataArray.length > 12) {
-            window.alert("Stack out of range")
-        }
-        else {
-
-        document.getElementById('pushingData').innerHTML = value
-        createElement(value)
-        this.dataArray.push(value)
-        movePushingElement() 
-        }
+  push(value) {
+    if (value == 0) {
+      errorBox.style.display = "block";
+      return;
+    } else {
+      errorBox.style.display = "none";
     }
 
-    pop() {
-        movePopingData()
-        const list = document.getElementsByClassName(structure)
-        list[0].remove()
-        document.getElementById('popingData').innerHTML = this.dataArray.pop()
-       
+    if (this.dataArray.length > 12) {
+      errorBox.style.display = "block";
+      errorBox.innerText = "Stack is Full";
+    } else {
+      document.getElementById("pushingData").innerHTML = value;
+      createElement(value);
+      this.dataArray.push(value);
+      movePushingElement();
     }
+  }
 
-    reset = function() {
-        container.innerHTML = ''    
-        this.dataArray = new Array();
-        document.getElementById('error').style.display = 'none';
-        
+  pop() {
+    if (this.dataArray.length == 0) {
+      errorBox.style.display = "block";
+      errorBox.innerText = "Stack is Empty";
+      return;
     }
+    movePopingData();
+    const list = document.getElementsByClassName(structure);
+    list[0].remove();
+    document.getElementById("popingData").innerHTML = this.dataArray.pop();
+  }
 
-    peek() {
-        const list = document.getElementsByClassName(structure)
-        document.getElementById('popingData').innerHTML = this.dataArray[this.dataArray.length-1]
+  reset = function () {
+    container.innerHTML = "";
+    this.dataArray = new Array();
+    errorBox.style.display = "none";
+  };
 
+  peek() {
+    if (this.dataArray.length == 0) {
+      errorBox.style.display = "block";
+      errorBox.innerText = "Stack is Empty";
+      return;
     }
-
-    
+    const list = document.getElementsByClassName(structure);
+    document.getElementById("popingData").innerHTML =
+      this.dataArray[this.dataArray.length - 1];
+  }
 }
 
 // Function that will be used by the browser
-let stack = new Stack()
-
-
-
-
-
+let stack = new Stack();
