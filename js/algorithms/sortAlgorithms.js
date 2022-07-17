16bef86d6ad2204f040e8a2c5517aa6c0bfe278c
Akshaj <akshajsr000@gmail.com>
2022-07-17 15:20:16 +0530

    <enhancement>[style sort]

--- js/algorithms/sortAlgorithms.js
+++ js/algorithms/sortAlgorithms.js
@@ -155,23 +155,27 @@ class SortAlgorithms{
             this.display.innerHTML = `<p style="color: white; font-size: large">Temp-> ${temp}</p>`
             j = i - 1;
             while (j >= 0 && this.array[j] > temp) {
-                if(i>=j+2) {
-                    this.container.children[j + 2].style.backgroundColor = 'lightgreen';
-                    this.container.children[j + 1].style.backgroundColor = 'lightblue';
-                }
+                this.display.innerHTML = `<p style="color: white; font-size: large">Temp-> ${temp}</p>`
                 this.container.children[i].style.backgroundColor = 'orange';
+                if(i>=j+2) {
+                    this.container.children[j + 1].style.backgroundColor = 'lightblue';
+                    this.container.children[j + 2].text = `<p style="color: black; font-size: xx-large">${temp}</p>`;
+                }
                 this.display.style.visibility = 'visible';
-                await sleep(this.speed);
                 this.array[j + 1] = this.array[j];
-                this.container.children[j].style.backgroundColor = 'lightgreen';
-                this.container.children[j+1].style.backgroundColor = 'lightblue';
+                this.container.children[j + 1].style.backgroundColor = 'lightblue';
+                this.container.children[j + 1].innerHTML = `<p style="color: black; font-size: xx-large">${temp}</p>`;
                 j = j - 1;
                 await sleep(this.speed);
                 this.render();
             }
             this.array[j + 1] = temp;
+            this.container.children[i].style.backgroundColor = 'orange';
+            this.container.children[j + 1].innerHTML = `<p style="color: black; font-size: xx-large">${temp}</p>`;
+            this.container.children[j + 1].style.backgroundColor = 'lightblue';
+            await sleep(this.speed);
             this.container.children[j + 1].style.backgroundColor = 'green';
-            await sleep(this.speed/2);
+            await sleep(this.speed);
             this.render()
         }
     }
