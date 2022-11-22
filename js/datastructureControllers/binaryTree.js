3e6530bcf8c6aaf90bf38fb8a8c4531b7ff672c6
Aashray Katiyar <aashray446@gmail.com>
2022-11-22 19:27:37 +0530

    <Fix> : [Binary Tree page scroll bug]

--- js/datastructureControllers/binaryTree.js
+++ js/datastructureControllers/binaryTree.js
@@ -1,35 +1,64 @@
-import { getRandomInt, sleep} from "../utils.js";
+// import { getRandomInt, sleep} from "../utils.js";
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function sleep(ms) {
+    return new Promise(
+        resolve => setTimeout(resolve, ms)
+    );
+}
 
 const treeContainer = document.getElementById('tree');
 let bst;
 
+//Input Fields
+const allInputFields = document.querySelectorAll("input[type='number']");
+
+//Error Indicator
+const errorBox = document.getElementById("error1");
+
 // Controller Buttons
-const createBtn = document.getElementById("createBtn")
-const insertBtn = document.getElementById("insertBtn")
-const searchBtn = document.getElementById("searchBtn")
-const randomBstBtn = document.getElementById("randomBstBtn")
-const resetBtn = document.getElementById("resetBtn")
+const createBtn = document.getElementById("createBtn");
+const insertBtn = document.getElementById("insertBtn");
+const searchBtn = document.getElementById("searchBtn");
+const randomBstBtn = document.getElementById("randomBstBtn");
+const resetBtn = document.getElementById("resetBtn");
 
 // Inputs 
-const createInput =  document.getElementById('create')
-const inserInput =  document.getElementById('insert')
-const searchInput = document.getElementById('search')
+const createInput = document.getElementById('create');
+const insertInput = document.getElementById('insert');
+const searchInput = document.getElementById('search');
 
 const createValue = (value) => {
-	const li = document.createElement('li');
-	const a = document.createElement('a');
-	a.innerText = value;
-	li.appendChild(a);
-	return li;
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.innerText = value;
+    li.appendChild(a);
+    return li;
 }
 
-class binarySearchTree {
+const clearAllInputFields = () => {
+    allInputFields.forEach(field => {
+        field.value = '';
+    });
+};
+
+function reset() {
+    bst = null;
+    treeContainer.innerHTML = '';
+    errorBox.style.display = "none";
+    clearAllInputFields();
+}
+
+class BinarySearchTree {
 
 
     constructor(value, firstTime) {
         this.value = createValue(value);
 
-        if(firstTime == true) {
+        if (firstTime == true) {
             const roota = document.getElementById('tree');
             const ul = document.createElement('ul');
             ul.appendChild(this.value);
@@ -42,51 +71,54 @@ class binarySearchTree {
     }
 
     insert(value) {
-    if(value < parseInt(this.value.firstChild.innerText)) {
+        if (value < parseInt(this.value.firstChild.innerText)) {
 
-        if(this.left == null) {
-            this.left = new binarySearchTree(value);
-            if(this.right == null) {
-                this.left.value.classList.add('left');
-                const ul = document.createElement('ul')
-                ul.appendChild(this.left.value);
-                this.value.appendChild(ul);
-            }
-            else {
-                this.value.lastChild.prepend(this.left.value);
-            }
+            if (this.left == null) {
+                this.left = new BinarySearchTree(value);
+                if (this.right == null) {
+                    this.left.value.classList.add('left');
+                    const ul = document.createElement('ul')
+                    ul.appendChild(this.left.value);
+                    this.value.appendChild(ul);
+                }
+                else {
+                    this.value.lastChild.prepend(this.left.value);
+                }
 
-        } else {
-            this.left.insert(value);
-        }
-    } else {
-        if(this.right == null  ) {
-            this.right = new binarySearchTree(value);
-            if(this.left == null) {
-                this.right.value.classList.add('right');
-                const ul = document.createElement('ul')
-                ul.appendChild(this.right.value);
-                this.value.appendChild(ul);
-            }
-            else {
-                this.value.lastChild.appendChild(this.right.value);
+            } else {
+                this.left.insert(value);
             }
         } else {
-            this.right.insert(value);
+            if (this.right == null) {
+                this.right = new BinarySearchTree(value);
+                if (this.left == null) {
+                    this.right.value.classList.add('right');
+                    const ul = document.createElement('ul')
+                    ul.appendChild(this.right.value);
+                    this.value.appendChild(ul);
+                }
+                else {
+                    this.value.lastChild.appendChild(this.right.value);
+                }
+            } else {
+                this.right.insert(value);
+            }
         }
+
     }
-
-}   
     //search in binary tree
-   async search(value) {
-        if(value == parseInt(this.value.firstChild.innerText)) {
+    async search(value) {
+        if (value == parseInt(this.value.firstChild.innerText)) {
             this.value.firstChild.classList.add('found');
             await sleep(4000);
             this.value.firstChild.classList.remove('found');
             return true;
         }
-        else if(value < parseInt(this.value.firstChild.innerText)) {
-            if(this.left == null) {
+        else if (value < parseInt(this.value.firstChild.innerText)) {
+            if (this.left == null) {
+                //Element not found
+                errorBox.style.display = "block";
+                errorBox.innerHTML = "Element not found!";
                 this.left.value.firstChild.classList.add('pointer');
                 await sleep(1000)
                 this.left.value.firstChild.classList.remove('pointer');
@@ -100,8 +132,10 @@ class binarySearchTree {
             }
         }
         else {
-            if(this.right == null) {
-               
+            if (this.right == null) {
+                //Element not found
+                errorBox.style.display = "block";
+                errorBox.innerHTML = "Element not found!";
                 this.right.value.firstChild.classList.add('pointer');
                 await sleep(1000)
                 this.right.value.firstChild.classList.remove('pointer');
@@ -114,50 +148,64 @@ class binarySearchTree {
                 return this.right.search(value);
             }
         }
-
-
-
     }
-
-    reset() {
-        treeContainer.innerHTML = '';
-    }
-        
-
 }
 
 
-
-
-const setBst = function(value) {
-    if(bst!= null) {
+const setBst = function (value) {
+    if (bst != null) {
         bst.reset();
         bst == null;
     }
-        bst = new binarySearchTree(value, true);
+    bst = new BinarySearchTree(value, true);
 }
 
-const generateRandomBst = function() {
-    if(bst != null) {
+const generateRandomBst = function () {
+    if (bst != null) {
         bst.reset();
     }
-    bst = new binarySearchTree(getRandomInt(0, 100), true);
-    for(let i = 0; i < 15; i++){
+    bst = new BinarySearchTree(getRandomInt(0, 100), true);
+    for (let i = 0; i < 15; i++) {
         bst.insert(getRandomInt(0, 100));
     }
 }
 
 
 // Button Event Listners Setup
-createBtn.addEventListener('click', ()=>{
-    setBst(createInput.value)
-})
-insertBtn.addEventListener('click', () => { 
-    bst.insert(inserInput.value)
-})
+// createBtn.addEventListener('click', ()=>{
+//     setBst(createInput.value)
+// });
+
+insertBtn.addEventListener('click', () => {
+    if (insertInput.value === '') {
+        //Don't allow show error and return
+        errorBox.style.display = "block";
+        errorBox.innerHTML = "Value cannot be blank!";
+        return;
+    } else {
+        errorBox.style.display = "none";
+    }
+    if (bst == null) {
+        bst = new BinarySearchTree(insertInput.value, true);
+    } else {
+        bst.insert(insertInput.value)
+    }
+});
 searchBtn.addEventListener('click', () => {
-    bst.search(searchInput.value)
-})
+    if (searchInput.value === '') {
+        //Don't allow show error and return
+        errorBox.style.display = "block";
+        errorBox.innerHTML = "Value cannot be blank!";
+        return;
+    } 
+    if (!bst) {
+        errorBox.style.display = "block";
+        errorBox.innerHTML = "Tree is empty!";
+        return;
+    }
+    errorBox.style.display = "none";
+    bst.search(parseInt(searchInput.value));
+});
 randomBstBtn.addEventListener('click', () => generateRandomBst())
-resetBtn.addEventListener('click', () => bst.reset())
+resetBtn.addEventListener('click', reset);
 
